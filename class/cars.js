e4c09daf611ef895f35d94570ebb6d7e3ed4ecd5
anifa <anifabazubagira@gmail.com>
2024-12-20 12:03:10 +0200

    Cars with their details using class in ES6

---
+++ class/cars.js
@@ -0,0 +1,19 @@
+class cars{
+    constructor(name,years,city){
+        this.name=name
+        this.years=years
+        this.city=city
+    }
+    method1 (){
+        console.log(`${this.name} was the first car in ${this.years} in ${this.city} city `)
+    }
+    method2(){
+        console.log(` ${this.name} was the second car in ${this.years} in ${this.city} city`)
+    }
+   
+}
+let first = new cars('Toyota','2003','Kigali')
+let second = new cars('Rav4','2010','Musanze')
+
+first.method1()
+second.method2()
