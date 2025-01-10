07114f7673b2fc5fb2d46dc42977934306b469cf
anifa <anifabazubagira@gmail.com>
2025-01-10 12:06:36 +0200

    Object.keys or Object.values methods that helps to acess the keys or values of object

---
+++ class/objectKeys.js
@@ -0,0 +1,12 @@
+const houses={
+    size:76,
+    table:5,
+}
+let array = Object.keys(houses)
+for(let parts of array){
+    console.log(parts)
+}
+let arrays = Object.values(houses)
+for(let parts of arrays){
+    console.log(parts)
+}
