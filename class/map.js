09356dacc1b3809effc7d9c715e382839b2c3006
Anifa <anifabazubagira@gmail.com>
2025-10-06 13:58:58 +0200

    learning map

--- class/map.js
+++
@@ -1,5 +0,0 @@
-let student = new Map()
-student.set('student',{name:"anifa",kg:23117})
-student.set('students',{name:"anifa",kg:23117})
-console.log(student)
-//key must be different so that you can be able to add other data
